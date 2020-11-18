b9131c9d6911c8d8b25a9e36e778a31e36a6f849
punitbudania <punitbudania88@gmail.com>
2020-11-18 18:20:06 +0530

    [punit]: working days added

--- EmpWageBuilderUC.java
+++ EmpWageBuilderUC.java
@@ -3,27 +3,34 @@ public class EmpWageBuilderUC{
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
-	
+	public static final int NUM_OF_WORKING_DAYS = 5;
+
 	public static void main(String args[]){
 //		System.out.println("*****Welcome To Employee Details*****");
 		//VARIABLES
 		int empHrs = 0;
 		int empWage = 0;
+		int totalWage = 0;
 		//cOMPUTATION
-		int empCheck = (int) Math.floor(Math.random()*10)%3;
-			
-		switch (empCheck)
-			{
-			case IS_FULL_TIME:
-				empHrs = 8;
-				break;
-			case IS_PART_TIME:
-				empHrs = 4;
-				break;
-			default:
-				empHrs = 0;
-			}
-		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Emp Wage: "+empWage);
+		for (int day = 0; day<NUM_OF_WORKING_DAYS; day++)
+		{
+			int empCheck = (int) Math.floor(Math.random()*10)%3;
+
+			switch (empCheck)
+				{
+				case IS_FULL_TIME:
+					empHrs = 8;
+					break;
+				case IS_PART_TIME:
+					empHrs = 4;
+					break;
+				default:
+					empHrs = 0;
+				}
+			empWage = empHrs * EMP_RATE_PER_HOUR;
+			totalWage += empWage;
+			System.out.println("Emp Wage: "+empWage);
+		}
+		System.out.println("total Emp wage = " + totalWage);
 	}
 }
